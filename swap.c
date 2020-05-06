0bafb3ba7489752180de0653c0b68d7e1d8384f9
ShenbagaMeenakshi <64904303+ShenbagaMeenakshi@users.noreply.github.com>
2020-05-06 17:55:51 +0530

    created swap.c
    this file is swapping of numbers

---
+++ swap.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int x,y;
+printf("Enter the value of x:");
+scanf("%d",&x);
+printf("\nEnter the value of y:");
+scanf("%d",&y);
+int temp=x;
+x=y;
+y=temp;
+printf("\nAfter swapping:x=%d,y=%d",x,y);
+return 0;
+}
